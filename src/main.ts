622ea7cb812aca1a8a4c2df45155f3c5eb5c9d09
updowncode <updowncode@gmail.com>
2018-06-21 13:09:58 -0400

    77

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { AppModule } from './app/app.module';
+
 if (environment.production) {
   enableProdMode();
 }
-
+if (environment.production) {
+  enableProdMode();
+}
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
-
