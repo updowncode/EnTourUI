4d6565503c6bda3a9d95f5c32b592bea4bbfbe20
updowncode <updowncode@gmail.com>
2018-06-21 12:59:37 -0400

    456

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
