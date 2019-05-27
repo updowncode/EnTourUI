92d346ef95794526655e783b562661422c1fed90
updowncode <updowncode@gmail.com>
2019-05-27 17:20:27 -0400

    123

--- src/app/en-tour.service.ts
+++ src/app/en-tour.service.ts
@@ -58,7 +58,7 @@ export class EnTourService implements OnDestroy {
   private tour: Tour;
   private trip: Trip;
 
-private siteIPToPublish = 'http://192.168.168.117:8019';
+private siteIPToPublish = 'http://dnndev.me';
   // private toursUrl = "http://localhost:51796/api/entours"; // URL to web api
   // private bookUrl = "http://localhost:51796/api/bookentour"; // URL to web api
 
